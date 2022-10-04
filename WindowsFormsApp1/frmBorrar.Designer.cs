6ca19eb9dfe8a8776899ffc315ced0ec7c3be47b
AngelLaines <54160330+AngelLaines@users.noreply.github.com>
2022-10-03 18:07:24 -0700

    color changed

--- WindowsFormsApp1/frmBorrar.Designer.cs
+++ WindowsFormsApp1/frmBorrar.Designer.cs
@@ -392,6 +392,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1067, 554);
             this.ControlBox = false;
             this.Controls.Add(this.panel5);
